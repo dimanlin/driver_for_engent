6f8f9d281d5ce8031a8dc27f091c8feaf29d0d03
Dmitry khromov <devandart@ya.ru>
2015-01-25 07:57:58 +0400

    init

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'driver_for_engent' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
